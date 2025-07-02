70c7afc44ed8c45a96234c533bf374b1ecf669cc
imgayathri <94066513+imgayathri@users.noreply.github.com>
2025-07-02 11:41:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   if (screen !== "splash") {
     return (
       <div>
-      <Router>
+      
         <Routes>
           <Route
             path="/"
@@ -31,7 +31,6 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-        </Router>
         </div>
       
     );
